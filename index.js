20bab884950502f466cad92c01ae727b50810ed8
tony-brian <btoni3823@gmail.com>
2023-07-04 12:19:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+  
+let equation = "x * y = 62";
 
-// Write your code here
+let randomInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let equation = "(n % m) === 4";
+
+let set = [10, 5, 20, 15];
+let maxNumber = set.reduce((max, current) => {
+  return Math.max(max, current);
+}, 0);
